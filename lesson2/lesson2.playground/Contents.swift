1ef5d07f6e6cbc7f17b8443dfbeef2fbd2f66212
klintukhov <76727787+klintukhov@users.noreply.github.com>
2022-11-22 20:32:04 +0100

    Add files via upload

---
+++ lesson2/lesson2.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var stringOne = "letter"
+
+var stringTwo: String = "letters"
+
+var emptyString = ""
+var emptyStringTwo = String()
+
+var a = ""
+var b: String = String()
+
+a = "Hello"
+b = "World"
+a + " " + b
+
+var number = 10
+print ("number \(number)")
+
+var char: Character = "X"
+
+stringOne + String(char)
+
+stringOne.append(char)
+stringOne.append(" A")
+stringTwo.append(a)
