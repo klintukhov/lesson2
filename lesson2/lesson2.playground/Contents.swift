1aea922d5a2d03124f7f615c8a151ee58c58bc86
klintukhov <76727787+klintukhov@users.noreply.github.com>
2022-11-26 20:56:31 +0100

    Update Contents.swift

--- lesson2/lesson2.playground/Contents.swift
+++ lesson2/lesson2.playground/Contents.swift
@@ -59,6 +59,96 @@ if peremString .isEmpty {
     print ("not empty")
 }
 
+// Practike SWIFTBOOK
+
+
+var charX = "anotherChar" // true
+
+let thumbsUp = "\u{1f44d}" // " "
+let blackSkin = "\u{1f3fd}" // "
+let combine = "\u{1f44d}\u{1f3fd}" // " "
+
+let cafeSimple = "caf\u{E9}" //
+"café"; let cafeCombine = "cafe\u{301}" //
+"café"; cafeSimple.count // 4
+cafeCombine.count // 4
+
+let nameE = "e\u{301}lastic" // "élastic"
+let index = nameE.startIndex
+
+let singleLineString = "These are the same."
+
+let multilineString = """
+
+These are the same.
+
+"""
+
+
+//Если вам нужна строка, которая занимает несколько строк, используйте многострочный строковый литерал - последовательность символов, заключенная в три двойные кавычки:Если вам нужна строка, которая занимает несколько строк, используйте многострочный строковый литерал - последовательность символов, заключенная в три двойные кавычки:
+
+let quotation = """
+
+The White Rabbit put on his spectacles. "Where shall I begin,
+
+please your Majesty?" he asked.
+
+
+
+"Begin at the beginning," the King said gravely, "and go on
+
+till you come to the end; then stop."
+
+"""
+
+/*Специальные символы в строковых литералах """
+Строковые литералы могут включать следующие специальные символы:
+
+Экранированные специальные символы \0(нулевой символ), \\(обратная косая черта), \t(горизонтальная табуляция), \n(перевод строки), \r(возврат каретки), \"(двойная кавычка) и \'(одинарная кавычка)
+Произвольное скалярное значение Unicode, записываемое как \u{n} , где n - шестнадцатеричное число от 1 до 8 цифр (Unicode обсуждается в Unicode ниже)
+*/
+
+//Код ниже показывает четыре примера этих специальных символов. wiseWordsКонстанта содержит два сбежавших двойные кавычки. Константы dollarSign, blackHeartи, sparklingHeartдемонстрируют скалярный формат Unicode:
+
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+
+// "Imagination is more important than knowledge" - Einstein
+
+let dollarSign = "\u{24}" // $, Unicode scalar U+0024
+
+let blackHeart = "\u{2665}" // ♥, Unicode scalar U+2665
+
+let sparklingHeart = "\u{1F496}" // 
+
+//Если вы хотите использовать разрывы строк, чтобы облегчить чтение исходного кода, но не хотите, чтобы разрывы строк были частью значения строки, напишите \в конце этих строк обратную косую черту ( ):
+let softWrappedQuotation = """
+
+The White Rabbit put on his spectacles. "Where shall I begin, \
+
+please your Majesty?" he asked.
+
+
+
+"Begin at the beginning," the King said gravely, "and go on \
+
+till you come to the end; then stop."
+
+"""
+
+//Чтобы сделать многострочный литерал, начинающийся или заканчивающийся переводом строки, напишите пустую строку в качестве первой или последней строки. Например:
+let lineBreaks = """
+
+
+
+This string starts with a line break.
+
+It also ends with a line break.
+
+
+
+"""
+
+
 //   1) Посоздавайте свои строки
 
 var stringEmpty = ""
